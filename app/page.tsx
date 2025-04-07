adae31226e2be245c4f5d5517256ad0b2048a6e6
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-04-07 03:03:56 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import About from "@/components/About";
 import BackgroundImage from "@/components/BackgroundImage";
 import Cardlist from "@/components/Cardlist";
-import Cards1 from "@/components/Cards1";
 import Cards2Kosina from "@/components/Cards2Kosina";
 import HeroOneImg from "@/components/HeroOneImg";
 import PozoviteNasOdmahImage from "@/components/PozoviteNasOdmahImage";
-import Testemonials from "@/components/Testimonials";
 import VideoCard from "@/components/VideoCard";
-import { cards1Data, cards2Data, cards2DataText } from "@/constants/index";
+import { cards2Data, cards2DataText } from "@/constants/index";
 
 export default function Home() {
   return (
